0d92727072341e554d4a46c4c7cec1f0b9f76c0d
Dan Stoppelman <dstop75@gmail.com>
2015-01-31 14:46:54 -0500

    Create one-to-many relationship List-to-Tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150131190345) do
+ActiveRecord::Schema.define(version: 20150131191144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 20150131190345) do
     t.boolean  "done?"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "list_id"
   end
 
+  add_index "tasks", ["list_id"], name: "index_tasks_on_list_id", using: :btree
+
+  add_foreign_key "tasks", "lists"
 end
