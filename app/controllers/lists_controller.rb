986f8d260d4b3416a582fec2230ed6fc397dbdf9
KBondanza <kelly.bondanza@gmail.com>
2015-01-31 15:43:08 -0500

    Filled out controller sections for lists and tasks.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,50 @@
 class ListsController < ApplicationController
 
+  before_action :find_list, only: [:show, :edit, :update, :destroy]
   def index
+    @lists = List.order(:title)
   end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @list.update_attributes(list_params)
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to action: :index
+  end
+
+  private
+
+  def find_list
+    @list = List.find(params[:id])
+  end
+  def list_params
+    params.require(:list).permit(:name, :category, :part_of_project, :progress)
+  end
+
 
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,50 @@
 class TasksController < ApplicationController
 
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
   def index
+    @tasks = Task.order(:title)
   end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update_attributes(task_params)
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to action: :index
+  end
+
+  private
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+  def task_params
+    params.require(:task).permit(:name, :completion_status)
+  end
+
 
 end
