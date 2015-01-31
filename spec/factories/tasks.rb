8e19ae0b75b3a3a479222375550d666118a90922
Dan Stoppelman <dstop75@gmail.com>
2015-01-31 14:02:33 -0500

    Add Task MVC and run migration

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :task do
+    action "MyString"
+done? false
+  end
+
+end
---
+++ spec/views/tasks/index.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "tasks/index", :type => :view do
+  before(:each) do
+    assign(:tasks, [
+      Task.create!(
+        :action => "Action",
+        :done? => false
+      ),
+      Task.create!(
+        :action => "Action",
+        :done? => false
+      )
+    ])
+  end
+
+  it "renders a list of tasks" do
+    render
+    assert_select "tr>td", :text => "Action".to_s, :count => 2
+    assert_select "tr>td", :text => false.to_s, :count => 2
+  end
+end
