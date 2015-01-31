50b24e6de1b7c9d42ce734a8e673ae8bd6f9ba31
KBondanza <kelly.bondanza@gmail.com>
2015-01-31 18:20:18 -0500

    Re-seeded database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-Project.create(
-  title: "ToDo App",
-  due_date: "2015/02/02",
-  progress: "started")
-
-List.create(
-  name: "ToDo App HW",
-  category: "Professional",
-  part_of_project: true,
-  progress: "started")
-
 Task.create(
   name: "Create Database",
   completion_status: true)
