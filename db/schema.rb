a3b1ee51bed372b3b54c632a88e69d521d4f2fac
KBondanza <kelly.bondanza@gmail.com>
2015-01-31 11:13:48 -0500

    Created the tasks model and did the migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150131160606) do
+ActiveRecord::Schema.define(version: 20150131161243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,5 +36,16 @@ ActiveRecord::Schema.define(version: 20150131160606) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "completion_status"
+    t.integer  "list_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
+
+  add_index "tasks", ["list_id"], name: "index_tasks_on_list_id", using: :btree
+
   add_foreign_key "lists", "projects"
+  add_foreign_key "tasks", "lists"
 end
---
+++ spec/factories/tasks.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :task do
+    name "MyString"
+completion_status false
+list nil
+  end
+
+end
