66fa19ee794cc2202e0d84fa1dfc5fdad8f66759
Dan Stoppelman <dstop75@gmail.com>
2015-01-31 15:20:03 -0500

    Add test data to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+List.delete_all
+groceries = List.create!(name: 'Groceries')
+laundry = List.create!(name: 'Laundry')
+
+Task.delete_all
+buy_milk = groceries.tasks.create!(action: 'buy milk', done?: false)
+buy_eggs = groceries.tasks.create!(action: 'buy eggs', done?: false)
+buy_butter = groceries.tasks.create!(action: 'buy butter', done?: true)
+
+wash_clothes = laundry.tasks.create!(action: 'wash clothes', done?: true)
+wash_towels = laundry.tasks.create!(action: 'wash towels', done?: true)
+wash_sheets = laundry.tasks.create!(action: 'wash sheets', done?: false)
+
+Note.delete_all
+organic = buy_milk.notes.create!(message: 'Organic')
+skim = buy_milk.notes.create!(message: 'Skim')
+
+sort = wash_clothes.notes.create!(message: 'Separate the whites')
