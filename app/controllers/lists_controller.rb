7ccd344ff684373345eae60c7cc0c8dbb6224a53
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-01-31 15:32:49 -0500

    Create new view and button to edit, update form to reflect a validation error

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,18 +6,37 @@ class ListsController < ApplicationController
   end
 
   def new
+     @list = List.new
+  end
+
+  def edit
+    @list = List.find(params[:id])
 
   end
 
   def create
     @list = List.new(list_params)
-    @list.save
-    redirect_to @list
+      if @list.save
+        redirect_to @list
+      else
+        render 'new'
+      end
   end
 
-private
-  def list_params
-    params.require(:list).permit(:title)
+  def update
+    @list = List.find(params[:id])
+
+      if @list.update(list_params)
+        redirect_to @list_params
+      else
+        render 'edit'
+      end
   end
+
+  private
+
+    def list_params
+      params.require(:list).permit(:title)
+    end
 
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,5 @@
 class List < ActiveRecord::Base
+  # has_many :tasks
+
+  validates :title, presence: true
 end
