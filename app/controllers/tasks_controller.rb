92e3469a340d92d16d4192a82636f32c2dd2a3fc
Annie <anne.bourgeois1@gmail.com>
2015-01-31 11:06:56 -0500

    Got views working for task list and the radio button for complete/incomplete

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,12 @@
+class TasksController < ApplicationController
+  def index
+    @list = List.find(params[:list_id])
+    @tasks = @list.tasks
+  end
+
+  def new
+    @list = List.find(params[:list_id])
+    @tasks = @list.tasks
+    @task = @list.tasks.new
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,32 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# List.delete_all
+
+errands = List.create!(name: "Errands")
+work = List.create!(name: "Work Stuff")
+correspondence = List.create!(name: "Correspondence")
+
+# Task.delete_all
+dry_cleaning = errands.tasks.create!(name: 'Pick up dry cleaning', status: false)
+wash_car = errands.tasks.create!(name: 'Wash the car', status: false)
+project = work.tasks.create!(name: "My big project", status: false)
+emails = correspondence.tasks.create!(name: "Send emails", status: false)
+phone_calls = correspondence.tasks.create!(name: "Phone calls", status: false)
+
+# Notes.delete_all
+
+dry_cleaning.notes.create!(name: "shirt")
+dry_cleaning.notes.create!(name: "jacket")
+wash_car.notes.create!(name: "use soap")
+project.notes.create!(name: "schedule meeting")
+project.notes.create!(name: "team review")
+emails.notes.create!(name: "Mary")
+emails.notes.create!(name: "Joe")
+phone_calls.notes.create!(name: "my favorite vendor")
+phone_calls.notes.create!(name: "interview candidate")
