452616d6c819f5a43b4bf19fc53ce6e75bb508af
KBondanza <kelly.bondanza@gmail.com>
2015-01-31 18:03:03 -0500

    Got very frustrated. Nothing was working. Tried to do things at once. So I deleted pretty much everything. So I'm starting over.

--- app/models/task.rb
+++
@@ -1,4 +0,0 @@
-class Task < ActiveRecord::Base
-  belongs_to :list
-  has_many :notes, dependent: :destroy
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,3 @@
 Rails.application.routes.draw do
 
-  resources :projects do
-    resources :lists
-  end
-
-  resources :lists do
-    resources :tasks
-  end
-
-  resources :tasks do
-    resources :notes
-  end
-
 end
