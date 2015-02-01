e54ca4e0c788ab6fd5155a2230141449d5a11490
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-02-01 13:46:31 -0500

    create views for task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,7 +8,31 @@ class TasksController < ApplicationController
   def new
     @list = List.find(params[:list_id])
     @task = @list.tasks.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.new(tasks_params)
+
+    if @task.save
+      redirect_to list_tasks_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update_attributes(params[:task])
+    redirect_to @task
 
   end
+
+  private
+
+    def tasks_params
+      params.require(:task).permit(:title)
+    end
+
 
 end
