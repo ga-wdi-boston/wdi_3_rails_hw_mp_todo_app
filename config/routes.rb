eb56ea99fac76e54cdcbaf744e4fa656a72973c5
Dan Stoppelman <dstop75@gmail.com>
2015-01-31 19:00:11 -0500

    Nest resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :notes
-
-  resources :tasks
-
-  resources :lists
+  resources :lists do
+    resources :tasks
+  end
+  resources :tasks do
+    resources :notes
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
