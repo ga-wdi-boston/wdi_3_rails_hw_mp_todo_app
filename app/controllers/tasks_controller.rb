1ddf5051968a2dc3ad963d736bf0ba3e8b015b27
KBondanza <kelly.bondanza@gmail.com>
2015-01-31 19:04:27 -0500

    Filled in views for tasks. Got everything for tasks to work.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
-def index
+class TasksController < ApplicationController
+
+  def index
     @tasks = Task.all
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root 'tasks#index'
   resources :tasks, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
