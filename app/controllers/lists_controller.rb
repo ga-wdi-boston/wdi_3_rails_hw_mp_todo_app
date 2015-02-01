af5c0d0dc2c9bc2f6bcf3e894aa78f24ad23f342
Ross <rdegnen@gmail.com>
2015-02-01 12:33:24 -0500

    edit and delete on lists working. although delete raisesa template missing error for some reason

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,10 +10,33 @@ class ListsController < ApplicationController
     @tasks = @list.tasks
   end
 
+  def edit
+    @project = Project.find(params[:project_id])
+    @list = @project.lists.find(params[:id])
+  end
+
   def create
     @project = Project.find(params[:project_id])
     @list = @project.lists.create(list_params)
     redirect_to project_path(@project)
+  end
+
+  def update
+    @project = Project.find(params[:project_id])
+    @list = @project.lists.find(params[:id])
+
+    if @list.update(list_params)
+      redirect_to @list.project
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:project_id])
+    @list = @project.lists.find(params[:id])
+
+    @list.destroy
   end
 
   private
