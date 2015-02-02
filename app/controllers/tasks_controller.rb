71f280da2d5423ac9798644df67f61227c7b68fc
Max Blaushild <blaushmild@gmail.com>
2015-02-02 15:47:25 -0500

    implemented check_box for task :completed on list#show and task#new, implemented destroy for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
 
   def create
     @list = List.find(params[:list_id])
-    @task = @list.tasks.new(tasks_params)
+    @task = @list.tasks.new(task_params)
 
     if @task.save
       redirect_to list_path(@list)
@@ -24,10 +24,33 @@ class TasksController < ApplicationController
     end
   end
 
+   def update
+    @task = Task.find(params[:id])
+    @task.update!(task_params)
+    redirect_to :back
+  end
+
+  # DELETE /Projects/1
+  # DELETE /Projects/1.json
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    respond_to do |format|
+      format.html { redirect_to list_url(@task.list_id), notice: 'Project was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_project
+      @project = Project.find(params[:id])
+    end
+
   private
 
-  def tasks_params
-    params.require(:task).permit(:name)
+  def task_params
+    params.require(:task).permit(:name, :completed)
   end
 
 end
