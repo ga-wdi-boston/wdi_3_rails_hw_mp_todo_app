4a91c78dedc4b0fafe1d0c48cc58820897bba2f5
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-02-02 15:55:32 -0500

    update controllers and views to show notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,28 @@
 class NotesController < ApplicationController
+
+  def index
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:task_id])
+    @notes = @task.notes
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:task_id])
+    @note = @task.notes.new(note_params)
+
+    if @note.save
+      redirect_to list_task_path(@list, @task, @note_id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:body)
+
+  end
 end
+
