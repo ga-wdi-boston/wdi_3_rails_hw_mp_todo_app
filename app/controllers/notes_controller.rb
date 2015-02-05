c344e569c4f43ce725697dfb8cf54b80481b37ea
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-02-04 23:15:10 -0500

    fixed edit and destroy actions for my note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,33 +6,50 @@ class NotesController < ApplicationController
     @notes = @task.notes
   end
 
+  def edit
+    # @task is beign assigned the object that was returned from the params hash using (params[:task_id])key of :task_id going to the dabatabe finding the Task table and getting the row with said ID and rails is  creating an object out of it.
+
+    # Get the id for the task from the params hash
+    # task_id = params[:task_id]
+    # # find the task in the DB with id of task_id
+    # # create an instance/object of Task from that row in the DB
+    # # assign that object to @task
+    # @task = Task.find(task_id)
+    @task = Task.find(params[:task_id])
+    # @note is beign assigned the object that was returned from the params
+    @note = @task.notes.find(params[:id])
+  end
+
   def create
     @list = List.find(params[:list_id])
     @task = Task.find(params[:task_id])
     @note = @task.notes.new(note_params)
 
     if @note.save
-      redirect_to list_task_path(@list, @task, @note_id)
+       redirect_to list_task_path(@list, @task, @note_id)
     else
       render :new
     end
   end
 
- # def destroy
- #    @list = List.find(params[:list_id])
- #    @task = Task.find(params[:task_id])
- #    @note = @task.notes.find(params[:id])
- #    @note.destroy
+ def update
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:task_id])
+    @note = @task.notes.find(params[:id])
 
- #    redirect_to list_task_path(@list, @task, @note_id)
- #  end
+      if @note.update(note_params)
+         redirect_to list_task_path(@list, @task, @note_id)
+      else
+        render :edit
+      end
+ end
 
- # def destroy
- #  @task = Task.find(params[:task_id])
- #  @note = @task.notes.find(params[:id])
- #  @note.destroy
- #    redirect_to task_notes_path(@task)
- # end
+ def destroy
+  @task = Task.find(params[:task_id])
+  @note = @task.notes.find(params[:id])
+  @note.destroy
+    redirect_to list_task_path(@task.list, @task)
+ end
 
   private
 
