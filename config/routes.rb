b75f5438e08cd5ee3e4981a62489e4e0fa0d65ae
Max Blaushild <blaushmild@gmail.com>
2015-02-05 10:28:59 -0500

    Refactored routes for shallow routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root 'projects#index'
+  shallow do
   resources :projects do
-    resources :lists
-  end
-  resources :lists do
-    resources :tasks
-  end
-  resources :tasks do
-    resources :notes
+    resources :lists do
+      resources :tasks do
+        resources :notes
+        end
+      end
+    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
